8b030fdb65cf2645d9322d163717421db3810cee
Katsuhiro Yamanaka <29446925+ogis-yamanaka@users.noreply.github.com>
2023-07-26 11:15:47 +0900

    ボタン名変更テスト

--- app/src/compornents/pages/cartPage.tsx
+++ app/src/compornents/pages/cartPage.tsx
@@ -146,7 +146,7 @@ const CartPage: React.FC = () => {
   return (
     <div className={classes.root}>
       <CartItem cart={data.cart} handleChange={itemCountChange} handleDelete={itemDelete} />
-      <NavigationButton forwardName="購入へ進む" forward={forward} backName="商品一覧へ戻る" back={back} />
+      <NavigationButton forwardName="購入確認へ" forward={forward} backName="商品一覧へ戻る" back={back} />
     </div>
   )
 }
